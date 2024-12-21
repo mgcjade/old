e5054c193b48079f02fe224f7b98003358fbb03e
mgcjade <149225848+mgcjade@users.noreply.github.com>
2024-12-21 08:57:48 +0800

    eqe

---
+++ gmoj/senior/problem/GMOJ_S_8408.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define fre(x) freopen(#x ".in", "r", stdin), freopen(#x ".out", "w", stdout)
+#define rep(i, a, b) for (int i = a; i <= b; i++)
+#define reb(i, a, b) for (int i = a; i >= b; i--)
+#define ref(i, a, b) for (int i = a; i < b; i++)
+using namespace std;
+typedef long long ll;
+
+const int N = 1e6;
+const int INF = 0x3f3f3f3f;
+const int MOD = 1e9 + 7;
+
+ll fast_pow(ll a, ll b)
+{
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll l, r;
+ll ans;
+ll t;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> l >> r;
+    cout << (fast_pow(2, r + 1) - fast_pow(2, l)) / 3 - (r - l + 1) / 2;
+    return 0;
+}
